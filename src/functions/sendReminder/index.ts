cabb77b402fe5e821fd559c5ca47921ef42107c1
Canberk <canberkkaya06@gmail.com>
2022-10-10 13:04:00 +0300

    feature: sendReminder function body

--- src/functions/sendReminder/index.ts
+++ src/functions/sendReminder/index.ts
@@ -1,8 +1,22 @@
 import { DynamoDBStreamEvent } from "aws-lambda";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { AttributeValue } from "@aws-sdk/client-dynamodb";
 
 export const handler = async (event: DynamoDBStreamEvent) => {
   try {
-    console.log(event);
+    const reminderPromises = event.Records.map(async (record) => {
+      const data = unmarshall(
+        record.dynamodb.OldImage as Record<string, AttributeValue>
+      );
+      const { email, phoneNumber, reminder } = data;
+      if (phoneNumber) {
+        await sendSMS({ phoneNumber, reminder });
+      }
+      if (email) {
+        await sendEmail({ email, reminder });
+      }
+    });
+    await Promise.all(reminderPromises);
   } catch (error) {
     console.log("error", error);
   }
